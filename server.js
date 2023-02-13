06dc2e0bba975d7c3929ec8b22fc77b3ea19537c
Felipe Fernandes <felipe-s-fernandes>
2023-02-13 20:11:47 -0300

    Servidor montado e ouvindo na porta 8080

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+config();
+
+// Importa o arquivo/módulo de rotas
+//import router from "./src/router.js";
+
+const PORT = process.env.SERVER_PORT;
+const HOSTNAME = process.env.SERVER_HOSTNAME;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+// Utiliza o 'router.js' como middleware
+//app.use(router);
+
+// Início do servidor
+app.listen(PORT, () => {
+    console.log(`Server running on http://${HOSTNAME}:${PORT}`);
+});
