e144d8cf328916a69391d29b81b46f71953d06d0
Felipe Fernandes <felipe.bravil@gmail.com>
2023-02-17 14:05:31 -0300

    Modificações na página inicial do projeto

--- public/pages/init/initPageModules.js
+++ public/pages/init/initPageModules.js
@@ -5,21 +5,19 @@ import { createElement } from "../../../modules/modules.js";
 
 export function renderButtonExplore() {
     const buttonInit = createElement("button", "buttonInit");
-    buttonInit.innerHTML = "Explorar";   
+    buttonInit.innerHTML = "Explorar";
 
     return buttonInit;
 }
 
 export function renderLogo() {
-
     const containerLogo = createElement("div", "containerLogo");
     const containerLogoAndWelcome = createElement("div", "divLogoAndWelcome");
     const welcome = createElement("span", "logo");
     const logoImage = createElement("img", "logo");
 
-    welcome.innerHTML = "Bem-vindo à";
+    welcome.innerHTML = "Bem-vindo a Whateverland!";
     logoImage.src = "../../uploads/logo.png";
-
 
     containerLogoAndWelcome.appendChild(welcome);
     containerLogoAndWelcome.appendChild(logoImage);
