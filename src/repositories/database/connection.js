e8b093e61395c8683a2b6e2ee75efd621a69462f
Anderson Lima <andersonlimabase@gmail.com>
2023-02-18 21:53:10 -0300

    Realizado alterações na conexão com o banco de dados para ficar genérica e criando um arquivo que conterá todas as queires

--- src/repositories/database/connection.js
+++ src/repositories/database/connection.js
@@ -1,20 +1,19 @@
 // Author: {Anderson Lima}
 
-async function connectDb() { 
+async function connectDb(query, arrayElements) { 
     try {
-        // await client.connect();
-
         await client.query("BEGIN");
 
-        const table = await client.query("query");
-        console.table(table.rows);
+        const result = await client.query(query, arrayElements);
+        console.table(result.rows);
 
         await client.query("COMMIT");
-
+        
+        return result.rows;
     } catch (error) {
+        // Tem que tratar o erro aqui
         console.log(error);
-        await client.query("ROLLBACK")
-        // await client.release();
+        await client.query("ROLLBACK");
     }
 }
 
