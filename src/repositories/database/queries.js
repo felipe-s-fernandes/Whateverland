3c32fc99c9af5822df907cd6d9c71f35b582dd89
Jônatas Gomes <jongaglima@gmail.com>
2023-02-20 17:40:03 -0300

    Updated queries 1.0.

--- src/repositories/database/queries.js
+++ src/repositories/database/queries.js
@@ -50,37 +50,38 @@ RETURNING region_id;
 
 // Insere dados de uma civilização específica.
 const postCivilization = `
-    INSERT INTO civilizations (region_id, civilization_name)
-    VALUES ($1, $2)
-    RETURNING civilization_id;
+INSERT INTO civilizations (region_id, civilization_name)
+VALUES ($1, $2)
+RETURNING civilization_id;
 `;
 
 // Insere dados de uma imagem na galeria.
 const postGallery = `
-    INSERT INTO gallery (civilization_id, gallery_image_title, image_unique_id)
-    VALUES ($1, $2, $3)
+    INSERT INTO gallery (civilization_id, gallery_image_title)
+    VALUES ($1, $2)
     RETURNING gallery_image_id;
 `;
 // Jonatas coloca tudo dentro de um objeto
 
 // Atualiza dados da start page de uma civilização específica.
 const patchStartPage = `
-    UPDATE start_pages
-    SET official_name = $2, localization = $3, capital = $4, religion = $5, government = $6, title = $7, paragraph = $8
-    WHERE civilization_id = $1;
+UPDATE start_pages
+SET official_name = $2, localization = $3, capital = $4, religion = $5, government = $6, title = $7, paragraph = $8
+WHERE civilization_id = $1;
 `;
 
 //Atualiza dados da history page de uma civilização específica.
 const patchHistoryPage = `
-    UPDATE history_pages
-    SET event_year = $2, event_title = $3, event_image = $4, event_image_label = $5, event_paragraph = $6
-    WHERE civilization_id = $1;
+UPDATE history_pages
+SET event_year = $2, event_title = $3, event_image = $4, event_image_label = $5, event_paragraph = $6
+WHERE civilization_id = $1;
 `;
+
 
 // Atualiza dados de uma imagem na galeria de uma civilização específica.
 const patchGallery = `
     UPDATE gallery
-    SET gallery_image_title = $3, image_unique_id = $4
+    SET gallery_image_title = $3
     WHERE civilization_id = $1 AND gallery_image_id = $2;
 `;
 
