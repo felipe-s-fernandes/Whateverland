467403def3a1fd5d776dad01e1973c91ea946705
Jônatas Gomes <jongaglima@gmail.com>
2023-02-20 15:43:00 -0300

    Added more queries and starting to use CSS variables.

--- src/repositories/database/queries.js
+++ src/repositories/database/queries.js
@@ -1,6 +1,12 @@
 // Autor: { Anderson Lima }
+// Co-author: {Jônatas Gomes}
+
 const getCivilizations = `
     SELECT * FROM civilizations WHERE region_id = $1;
+`;
+
+const getAllCivilizations = `
+    SELECT * FROM civilizations;
 `;
 
 const getGallery = `
@@ -25,7 +31,7 @@ const getRegionById = `
 `;
 
 const getHistory = `
-    SELECT * FROM history_events where civilization_id = $1;
+    SELECT * FROM history_events WHERE civilization_id = $1;
 `;
 
 const postCivilization = `
@@ -33,17 +39,32 @@ const postCivilization = `
     VALUES ($1, $2)
     RETURNING civilization_id;
 `;
-// Jonantas coloca tudo dentro de um objeto
+// Jonatas coloca tudo dentro de um objeto
+
+const postRegion = `
+    INSERT INTO regions(region_name, region_path)
+    VALUES ($1, $2) 
+    RETURNING region_id;
+`;
+
+const patchStart_page = `
+    UPDATE start_pages
+    SET official_name = $2, localization = $3, capital = $4, religion = $5, government = $6, title = $7, paragraph = $8
+    WHERE civilization_id = $1;
+`
 
 const query = {
     getCivilizations: getCivilizations,
+    getCivilizationById: getCivilizationById,
+    getAllCivilizations: getAllCivilizations,
     getGallery: getGallery,
     getRegions: getRegions,
     getRegionById: getRegionById,
     getHistory: getHistory,
-    postCivilization: postCivilization,
-    getCivilizationById: getCivilizationById,
     getStart: getStart,
+    postCivilization: postCivilization,
+    postRegion: postRegion,
+    patchStart_page: patchStart_page,
 };
 
 export default query;
