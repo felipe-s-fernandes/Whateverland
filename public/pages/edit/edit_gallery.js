f5ab080b5a71475019478b0c80e632548f2a2726
Felipe Fernandes <felipe.bravil@gmail.com>
2023-02-22 17:56:50 -0300

    Corrigido bug no delete da galeria

--- public/pages/edit/edit_gallery.js
+++ public/pages/edit/edit_gallery.js
@@ -4,25 +4,25 @@ import { createElement } from "../../modules/modules.js";
 import HTTPRequest from "../../modules/HTTPRequest.js";
 import imgRequest from "../../modules/imgRequest.js";
 
-
 // ***Requisições***
 
 // Requisição GET para renderizar a tabela a tabela
 export async function reqRenderTableGallery(civilizationId) {
-    const galleryObject = await HTTPRequest(`/gallery/${civilizationId}`, "GET");
+    const galleryObject = await HTTPRequest(
+        `/gallery/${civilizationId}`,
+        "GET"
+    );
 
     await renderTable(galleryObject.gallery);
 }
 
-export async function reqDeleteImage(civilizationId) {
-
-    await HTTPRequest(`/gallery/${civilizationId}`, "DELETE");
+export async function reqDeleteImage(imageId, civilizationId) {
+    await HTTPRequest(`/gallery/${imageId}`, "DELETE");
 
     const table = document.querySelector("#tableGallery");
     table.innerHTML = "";
     await reqRenderTableGallery(civilizationId);
 }
-
 
 // Formulário de preenchimento
 export function eventFormGallery(civilizationId) {
@@ -36,36 +36,32 @@ export function eventFormGallery(civilizationId) {
 
         // Requisitando para o servidor cadastrar o nova civilização no banco de dados
         // await newImageGallery(civilizationId, form.civi_gallery.value, form.img_gallery.value);
-        
-        
-        
+
         // Zona de edição
-        
+
         const formData = new FormData();
         const file = document.querySelector("#img_gallery");
-        
+
         // Adiciona a imagem ao FormData
         formData.append("file", file.files[0]);
-        
+
         // Adiciona as 4 strings ao FormData
         formData.append("civilization_id", civilizationId);
         formData.append("gallery_image_title", form.civi_gallery.value);
-        
+
         // Requisitando para o servidor cadastrar o nova civilização no banco de dados
         await imgRequest(`/gallery/`, "POST", formData);
-        
+
         // galleryObject.civilization_id,
         // galleryObject.gallery_image_title,
         // galleryObject.gallery_image_id,
 
         // Zona de edição
 
-
         reqRenderTableGallery(civilizationId);
         imageLegend.value = "";
     });
 }
-
 
 // Renderização da tabela que recebe o array de dados
 async function renderTable(array) {
@@ -97,13 +93,15 @@ async function renderTable(array) {
 
         column1.innerHTML = `${array[i].gallery_image_title}`;
         column2.innerHTML = `${array[i].image_unique_id}`;
-        column3.innerHTML =  `<img src="../../uploads/excluir.png" alt="Ícone de excluir">`;
+        column3.innerHTML = `<img src="../../uploads/excluir.png" alt="Ícone de excluir">`;
 
         // Eventos de editar e deletar dados da tabela
         // console.log(array[i]);
         // column4.addEventListener("click", () => redirectEditPage(array[i].civilization_id));
         // column2.addEventListener("click", () => userInput(array[i]));
-        column3.addEventListener("click", async () => reqDeleteImage(array[i].image_unique_id));
+        column3.addEventListener("click", async () =>
+            reqDeleteImage(array[i].image_unique_id, array[i].civilization_id)
+        );
 
         tableBody.appendChild(line);
     }
