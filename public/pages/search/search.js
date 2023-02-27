118838b67657a900e229ec99e578d2b515b14b49
Felipe Fernandes <felipe.bravil@gmail.com>
2023-02-27 12:37:12 -0300

    Ajustado bug na página de busca

--- public/pages/search/search.js
+++ public/pages/search/search.js
@@ -61,6 +61,9 @@ async function searchResults(string) {
         const resultFigure = createElement("figure", "resultFigure");
         const resultImg = createElement("img", "resultImg");
         resultImg.src = "../../uploads/" + civilization.civilization_image;
+        resultImg.onerror = () => {
+            resultImg.src = `../../uploads/default_image.jpg`;
+        };
         resultFigure.appendChild(resultImg);
 
         const resultArticle = createElement("article", "resultArticle");
