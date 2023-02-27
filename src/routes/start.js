d2612fe034942c5caf8f62100c19294d4d4a0ded
Felipe Fernandes <felipe.bravil@gmail.com>
2023-02-27 15:26:45 -0300

    Adicionada autenticação nas rotas POST e PATCH

--- src/routes/start.js
+++ src/routes/start.js
@@ -2,6 +2,7 @@
 // CoAutor {Felipe Fernandes}
 
 import express from "express";
+import authenticate from "../../middleware/authenticate.js";
 const startRouter = express.Router();
 import startController from "../controllers/start.js";
 
@@ -9,6 +10,6 @@ startRouter.get("/search/:string", startController.searchStart);
 
 startRouter.get("/:civilizationid", startController.getStart);
 
-startRouter.patch("/edit", startController.patchStart);
+startRouter.patch("/edit", authenticate, startController.patchStart);
 
 export default startRouter;
