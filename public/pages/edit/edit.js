03557381a553708fedd9449d554d4feb9577c167
Jônatas Gomes <jongaglima@gmail.com>
2023-02-27 23:48:31 -0300

    CSS update on edit page.

--- public/pages/edit/edit.js
+++ public/pages/edit/edit.js
@@ -28,6 +28,7 @@ import {
 
 export default async function RenderEditPage(civilizationId) {
     const container = createElement("div", "containerEditPages");
+    /* container.id="containerEditPages"; */
 
     const gambiarraContainer = createElement("div", "gambiarraContainer");
 
