52a80276454461b96f19ed7b6b83f1e04d037402
Anderson Lima <andersonlimabase@gmail.com>
2023-02-28 23:08:52 -0300

    Organização do código CSS da página inicial

--- public/pages/init/initPageModules.js
+++ public/pages/init/initPageModules.js
@@ -1,6 +1,5 @@
 //@author {Anderson Lima}
 
-
 import { createElement } from "../../../modules/modules.js";
 
 export function renderButtonExplore() {
@@ -30,22 +29,22 @@ export function clouds() {
 
     const allClouds = createElement("div", "containerClouds")
 
-    const cloud1 = createElement("img", "clouds1");
+    const cloud1 = createElement("img", "clouds");
     cloud1.style.setProperty('--i', '1');
     cloud1.src = "../../uploads/cloud1.png";
     allClouds.appendChild(cloud1);
     
-    const cloud4 = createElement("img", "clouds4");
+    const cloud4 = createElement("img", "clouds");
     cloud4.style.setProperty('--i', '9');
     cloud4.src = "../../uploads/cloud4.png";
     allClouds.appendChild(cloud4);
 
-    const cloud2 = createElement("img", "clouds2");
+    const cloud2 = createElement("img", "clouds");
     cloud2.style.setProperty('--i', '3');
     cloud2.src = "../../uploads/cloud2.png";
     allClouds.appendChild(cloud2);
     
-    const cloud3 = createElement("img", "clouds3");
+    const cloud3 = createElement("img", "clouds");
     cloud3.style.setProperty('--i', '6');
     cloud3.src = "../../uploads/cloud3.png";
     allClouds.appendChild(cloud3);
