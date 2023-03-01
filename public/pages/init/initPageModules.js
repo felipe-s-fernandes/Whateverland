2192835f6ff12617faa13a3376bfc17e29b3545e
Felipe Fernandes <felipe.bravil@gmail.com>
2023-02-28 22:57:06 -0300

    Removido 'Seja bem vindo a'

--- public/pages/init/initPageModules.js
+++ public/pages/init/initPageModules.js
@@ -1,6 +1,5 @@
 //@author {Anderson Lima}
 
-
 import { createElement } from "../../../modules/modules.js";
 
 export function renderButtonExplore() {
@@ -13,42 +12,38 @@ export function renderButtonExplore() {
 
 export function renderLogo() {
     const containerLogoAndWelcome = createElement("div", "divLogoAndWelcome");
-    const welcome = createElement("span", "logo");
     const logoImage = createElement("img", "logoImage");
-    
-    welcome.innerHTML = "Seja bem-vindo";
+
     logoImage.src = "../../uploads/logo.png";
 
-    containerLogoAndWelcome.appendChild(welcome);
     containerLogoAndWelcome.appendChild(logoImage);
 
     return containerLogoAndWelcome;
 }
 
 export function clouds() {
-
     // Nuvens
-    const allClouds = createElement("div", "containerClouds")
+    const allClouds = createElement("div", "containerClouds");
 
     const cloud1 = createElement("img", "clouds1");
-    cloud1.style.setProperty('--i', '1');
+    cloud1.style.setProperty("--i", "1");
     cloud1.src = "../../uploads/cloud1.png";
     allClouds.appendChild(cloud1);
-    
+
     const cloud4 = createElement("img", "clouds4");
-    cloud4.style.setProperty('--i', '9');
+    cloud4.style.setProperty("--i", "9");
     cloud4.src = "../../uploads/cloud4.png";
     allClouds.appendChild(cloud4);
 
     const cloud2 = createElement("img", "clouds2");
-    cloud2.style.setProperty('--i', '3');
+    cloud2.style.setProperty("--i", "3");
     cloud2.src = "../../uploads/cloud2.png";
     allClouds.appendChild(cloud2);
-    
+
     const cloud3 = createElement("img", "clouds3");
-    cloud3.style.setProperty('--i', '6');
+    cloud3.style.setProperty("--i", "6");
     cloud3.src = "../../uploads/cloud3.png";
     allClouds.appendChild(cloud3);
-    
-    return allClouds;    
+
+    return allClouds;
 }
